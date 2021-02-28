b2eb7609f40b3a4d137659876f811cf56f5680df
clairecw <clairewang@lyft.com>
2021-02-27 23:07:24 -0500

    ff base working

--- Space_Invaders_new/run_ff.py
+++ Space_Invaders_new/run_ff.py
@@ -1,10 +1,9 @@
 import pygame as pg
 import fam_feud as ff
-
+from bar import *
 
 COLOR_INACTIVE = pg.Color('lightskyblue3')
 COLOR_ACTIVE = pg.Color('dodgerblue2')
-FONT = pg.font.Font(None, 32)
 WHITE = (255, 255, 255)
 GREEN = (78, 255, 87)
 YELLOW = (241, 255, 0)
@@ -28,8 +27,8 @@ class InputBox:
         self.rect = pg.Rect(x, y, w, h)
         self.color = COLOR_INACTIVE
         self.text = text
-        self.txt_surface = FONT.render(text, True, self.color)
-        self.active = False
+        self.txt_surface = pg.font.Font(None, 32).render(text, True, self.color)
+        self.active = True
 
     def handle_event(self, event):
         ans = ''
@@ -37,9 +36,7 @@ class InputBox:
             # If the user clicked on the input_box rect.
             if self.rect.collidepoint(event.pos):
                 # Toggle the active variable.
-                self.active = not self.active
-            else:
-                self.active = False
+                self.active = True
             # Change the current color of the input box.
             self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
         if event.type == pg.KEYDOWN:
@@ -53,7 +50,7 @@ class InputBox:
                 else:
                     self.text += event.unicode
                 # Re-render the text.
-                self.txt_surface = FONT.render(self.text, True, self.color)
+                self.txt_surface = pg.font.Font(None, 32).render(self.text, True, self.color)
         return ans
 
     def update(self):
@@ -73,30 +70,36 @@ def main():
     pg.init()
     screen = pg.display.set_mode((640, 480))
     clock = pg.time.Clock()
-    input_box1 = InputBox(100, 100, 140, 32)
+    input_box1 = InputBox(10, 300, 140, 32)
     input_boxes = [input_box1]
-    bar_obj = Bar(screen)
+    bar_obj = Bar(screen, 5, 5)
     bar = pygame.sprite.GroupSingle(bar_obj)
     
     done = False
     feud = ff.FamFeud()
     idx, qn = feud.draw_next_q()
-    lastscoreText = TextBox(200, 50, 140, 32, text="")
+    lastscoreText = TextBox(500, 5, 140, 32, text="")
 
     while not done:
-        titleText = TextBox(20, 50, 140, 32, text=qn)
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                done = True
+        titleText = TextBox(20, 100, 140, 32, text=qn)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+                pygame.quit()
+            
             for box in input_boxes:
                 ans = box.handle_event(event)
                 if ans:
                     score, sols = feud.score_ans(idx, ans)
                     lastscoreText = TextBox(400, 5, 140, 32, text=str(score))
+                    if score > 0:
+                        bar.sprite.up(score)
+                    else:
+                        bar.sprite.down(5)
+
                     idx, qn = feud.draw_next_q()
-                    titleText = TextBox(20, 50, 140, 32, text=qn)
-
-
+                    titleText = TextBox(20, 100, 140, 32, text=qn)
 
         for box in input_boxes:
             box.update()
@@ -106,6 +109,8 @@ def main():
             box.draw(screen)
         titleText.draw(screen)
         lastscoreText.draw(screen)
+        bar.draw(screen)
+        bar.update()
 
         pg.display.flip()
         clock.tick(30)
