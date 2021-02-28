8a054b8763f7dd84a63bc501a9a8f32915f924e7
tyu91 <tiffanyy9968@gmail.com>
2021-02-27 23:15:19 -0500

    frozen temp

--- Space_Invaders_new/run_frozen_uber_model.py
+++ Space_Invaders_new/run_frozen_uber_model.py
@@ -6,6 +6,8 @@ import gym
 from control_map import *
 from bar import *
 import random
+from queue import Queue 
+from threading import Thread 
 
 """## Download trained model and precomputed rollout data"""
 
@@ -63,60 +65,16 @@ from gym.envs.classic_control import rendering
 def repeat_upsample(rgb_array, k=1, l=1, err=[]):
     return np.repeat(np.repeat(rgb_array, k, axis=0), l, axis=1)
 
-
-with tf.Graph().as_default() as graph, tf.Session(config=config) as sess:
-    if preprocessing == 'dopamine': #dopamine-style preprocessing
-        env = gym.make(m.environment)
-        if hasattr(env,'unwrapped'):
-            env = env.unwrapped
-        env = DopamineAtariPreprocessing(env)
-        env = FrameStack(env, 4)
-        env = ScaledFloatFrame(env,scale=1.0/255.0)
-    elif preprocessing == 'np': #use numpy preprocessing
-        env = gym.make(m.environment)
-        env = atari_wrappers.wrap_deepmind(env, episode_life=False,preproc='np')
-    else:  #use tensorflow preprocessing
-        env = gym.make(m.environment)
-        env = atari_wrappers.wrap_deepmind(env, episode_life=False,preproc='tf')
-
-    nA = env.action_space.n
-    X_t = tf.placeholder(tf.float32, [None] + list(env.observation_space.shape))
-
-    T = import_model(m,X_t,X_t)
-    action_sample = m.get_action(T)
-
-    #get intermediate level representations
-    activations = [T(layer['name']) for layer in m.layers]
-    high_level_rep = activations[-2] #not output layer, but layer before
-
-    sample_observations = []
-    sample_frames = []
-    sample_ram = []
-    sample_representation = []
-    sample_score = []
-    actions = []
-
-    obs = env.reset()
-
-    ep_count = 0
-    rewards = []; ep_rew = 0
-    frame_count = 0
-
-    prev_action = None
-
-    render = True
-    verbose = True
-
-    viewer = rendering.SimpleImageViewer()
-
+def bar_loop_shit(q):
     pygame.init()
     screen = pygame.display.set_mode((800,800))
     clock = pygame.time.Clock()
     bar_obj = Bar(screen)
     bar = pygame.sprite.GroupSingle(bar_obj)
-    
-    # Evaluate policy over test_eps episodes
-    while ep_count < 1:
+    randomize = False
+    prob = 0
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
@@ -133,55 +91,120 @@ with tf.Graph().as_default() as graph, tf.Session(config=config) as sess:
         pygame.display.update()
         clock.tick(10)
 
-        if render:
-            rgb = env.render('rgb_array')
-            upscaled = repeat_upsample(rgb,4, 4)
-            viewer.imshow(upscaled)
-            
-        train_dict = {X_t:obs[None]}
-        results = sess.run([action_sample,high_level_rep], feed_dict=train_dict)
-
-        #grab action
-        act = results[0]
-
         if bar_obj.randomize and random.uniform(0, 1) < bar_obj.prob:
-            act = np.array([env.action_space.sample()])
-            print(act)
-        actions.append(act[0])
+            randomize = bar_obj.randomize
+            prob = bar_obj.prob
+            # act = np.array([env.action_space.sample()])
+            # print(act)
+        else:
+            randomize = False
+            prob = 0.
 
-        # translate to amy
-        # eye_movement = act_to_eyes[act]
+        q.put((randomize, prob))
 
-        #get high-level representation
-        representation = results[1][0]
+def play_atari(q):
+    with tf.Graph().as_default() as graph, tf.Session(config=config) as sess:
+        if preprocessing == 'dopamine': #dopamine-style preprocessing
+            env = gym.make(m.environment)
+            if hasattr(env,'unwrapped'):
+                env = env.unwrapped
+            env = DopamineAtariPreprocessing(env)
+            env = FrameStack(env, 4)
+            env = ScaledFloatFrame(env,scale=1.0/255.0)
+        elif preprocessing == 'np': #use numpy preprocessing
+            env = gym.make(m.environment)
+            env = atari_wrappers.wrap_deepmind(env, episode_life=False,preproc='np')
+        else:  #use tensorflow preprocessing
+            env = gym.make(m.environment)
+            env = atari_wrappers.wrap_deepmind(env, episode_life=False,preproc='tf')
 
-        frame = env.render(mode='rgb_array')
-        sample_frames.append(np.array(frame,dtype=np.uint8))
-        sample_ram.append(env.unwrapped._get_ram())
-        sample_representation.append(representation)
-        sample_observations.append(np.array(obs))
+        nA = env.action_space.n
+        X_t = tf.placeholder(tf.float32, [None] + list(env.observation_space.shape))
 
-        sample_score.append(ep_rew)
+        T = import_model(m,X_t,X_t)
+        action_sample = m.get_action(T)
 
-        if prev_action != None and random.random() < sticky_action_prob:
-            act = prev_action
+        #get intermediate level representations
+        activations = [T(layer['name']) for layer in m.layers]
+        high_level_rep = activations[-2] #not output layer, but layer before
 
-        prev_action = act
+        sample_observations = []
+        sample_frames = []
+        sample_ram = []
+        sample_representation = []
+        sample_score = []
+        actions = []
 
-        obs, rew, done, info = env.step(np.squeeze(act))
+        obs = env.reset()
 
-        ep_rew += rew
-        frame_count+=1
+        ep_count = 0
+        rewards = []; ep_rew = 0
+        frame_count = 0
 
-        if done:
-            obs = env.reset()
-            ep_count += 1
-            rewards.append(ep_rew)
-            ep_rew = 0.
+        prev_action = None
 
-    if verbose:
-        print("Avg. Episode Reward: ", np.mean(rewards))
-        print("rewards:",rewards)
-        print("frames:",frame_count)
+        render = True
+        verbose = True
 
-    results = {'observations':sample_observations,'frames':sample_frames,'ram':sample_ram,'representation':sample_representation,'score':sample_score,'ep_rewards':rewards,'actions':actions}
+        viewer = rendering.SimpleImageViewer()
+        
+        # Evaluate policy over test_eps episodes
+        while ep_count < 1:
+            
+            if render:
+                rgb = env.render('rgb_array')
+                upscaled = repeat_upsample(rgb,4, 4)
+                viewer.imshow(upscaled)
+                
+            train_dict = {X_t:obs[None]}
+            results = sess.run([action_sample,high_level_rep], feed_dict=train_dict)
+
+            #grab action
+            act = results[0]
+
+            # if bar_obj.randomize and random.uniform(0, 1) < bar_obj.prob:
+            #     act = np.array([env.action_space.sample()])
+            #     print(act)
+            print(q.get())
+            actions.append(act[0])
+
+            #get high-level representation
+            representation = results[1][0]
+
+            frame = env.render(mode='rgb_array')
+            sample_frames.append(np.array(frame,dtype=np.uint8))
+            sample_ram.append(env.unwrapped._get_ram())
+            sample_representation.append(representation)
+            sample_observations.append(np.array(obs))
+
+            sample_score.append(ep_rew)
+
+            if prev_action != None and random.random() < sticky_action_prob:
+                act = prev_action
+
+            prev_action = act
+
+            obs, rew, done, info = env.step(np.squeeze(act))
+
+            ep_rew += rew
+            frame_count+=1
+
+            if done:
+                obs = env.reset()
+                ep_count += 1
+                rewards.append(ep_rew)
+                ep_rew = 0.
+
+        if verbose:
+            print("Avg. Episode Reward: ", np.mean(rewards))
+            print("rewards:",rewards)
+            print("frames:",frame_count)
+
+        results = {'observations':sample_observations,'frames':sample_frames,'ram':sample_ram,'representation':sample_representation,'score':sample_score,'ep_rewards':rewards,'actions':actions}
+
+q = Queue() 
+# t1 = Thread(target = play_atari, args =(q, )) 
+# t1.start() 
+
+bar_loop_shit(q)
+
