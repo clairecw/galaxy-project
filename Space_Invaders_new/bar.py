f70e482937973d641f960b3e25998737b37d5295
clairecw <clairewang@lyft.com>
2021-02-28 08:42:17 -0500

    merge

--- Space_Invaders_new/bar.py
+++ Space_Invaders_new/bar.py
@@ -10,7 +10,7 @@ class Bar(pygame.sprite.Sprite):
     def __init__(self, screen, x, y):
         super().__init__()
         self.image = pygame.Surface((40,40))
-        self.image.fill((200,30,30))
+        self.image.fill((30,30,30))
         self.rect = self.image.get_rect(center = (400,400))
         self.current_health = 200
         self.target_health = 500
@@ -67,27 +67,3 @@ class Bar(pygame.sprite.Sprite):
 
         self.randomize = True if transition_color == GREEN else False
         self.prob = transition_width / (self.health_bar_length // 2)
-
-
-
-# pygame.init()
-# screen = pygame.display.set_mode((800,800))
-# clock = pygame.time.Clock()
-# bar = pygame.sprite.GroupSingle(Bar())
-
-# while True:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             sys.exit()
-#             pygame.quit()
-#         if event.type == pygame.KEYDOWN:
-#             if event.key == pygame.K_UP:
-#                 bar.sprite.up(100)
-#             if event.key == pygame.K_DOWN:
-#                 bar.sprite.down(5)
-
-#     screen.fill((30,30,30))
-#     bar.draw(screen)
-#     bar.update()
-#     pygame.display.update()
-#     clock.tick(60)
--- Space_Invaders_new/control_map.py
+++ Space_Invaders_new/control_map.py
@@ -2,9 +2,9 @@ act_to_eyes = {
     0: 'center',
     1: 'left',
     2: 'right',
-    3: 'blink',
+    3: 'shoot',
     4: 'left',
     5: 'right'
 }
 
-eyes_to_act = {v: k for k, v in act_to_eyes.items()}
+eyes_to_act = {v: [k] for k, v in act_to_eyes.items()}
